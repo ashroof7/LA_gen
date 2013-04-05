e0572b6f0553de7888a77fb408a9f52738c520fe
leonardo7 <leo_aly7@ymail.com>
2013-04-05 15:47:04 +0200

    initial commit :D

---
+++ src/test.cpp
@@ -0,0 +1,17 @@
+/*
+ * test.cpp
+ *
+ *  Created on: Apr 5, 2013
+ *      Author: leo
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout<<"al3ab baleh"<<endl;
+	return 0;
+}
+
+
