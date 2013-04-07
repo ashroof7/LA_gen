010f80175fe55b140b69d81ab9bb7d6188001ef6
Mohamed Mustafa <mohamed_czar@yahoo.com>
2013-04-07 19:13:14 +0200

    dfa edit

--- src/dfa.cpp
+++ src/dfa.cpp
@@ -1,48 +1,32 @@
-#include <iostream>
-#include <cstdlib>
-#include <string>
-#include <iostream>
-#include <stdio.h>
-#include <math.h>
-#include <string>
-#include <fstream>
-#include <vector>
-#include <map>
-#include <stack>
-#include <sstream>
-#include <cstdlib>
-#include <fstream>
-#include <cstdlib>
-#include <stdio.h>
-#include <queue>
-#include <algorithm>
-
 #include "graph.h"
 #include "dfa.h"
+#include "base.h"
 
 using namespace std;
 
 //vector<int> dfa::epsilon_closure(int node);
 
 graph v_graph;
-vector< vector<int> > state_closure;
+vector<vector<int> > state_closure;
 
 dfa::dfa(graph gg) {
 	v_graph = gg;
 
 	// initialize epsilon closure of all states
-	for (int i = 0; i < v_graph.getLen() ; i++) {
+	for (int i = 0; i < v_graph.getLen(); i++) {
 		state_closure.push_back(epsilon_closure(i));
 	}
 }
 
 vector<int> dfa::epsilon_closure(int node) {
+
 	vector<int> result;
 	queue<int> q;
 	q.push(node);
-	vector<bool> mask;
+	bool mask[v_graph.getLen()];
+
 	for (int i = 0; i < v_graph.getLen(); i++) {
-		mask.push_back(false);
+		mask[i] = false;
 	}
 	mask[node] = true;
 
@@ -50,13 +34,12 @@ vector<int> dfa::epsilon_closure(int node) {
 		int n = q.front();
 		q.pop();
 		for (unsigned i = 0; i < v_graph.get_children(n).size(); i++) {
-			pair<int, char> p;
-			p.first = v_graph.get_pair(n, i).first;
-			p.second = v_graph.get_pair(n, i).second;
-			if (p.second == v_graph.EPSILON && !mask[p.first]) {
-				result.push_back(p.first);
-				q.push(p.first);
-				mask[p.first] = true;
+			pib edge = v_graph.get_pair(n, i);
+			int child = edge.first;
+			if (edge.second[EPISILON] == 1 && !mask[child]) {
+				result.push_back(child);
+				q.push(child);
+				mask[child] = true;
 			}
 		}
 	}
