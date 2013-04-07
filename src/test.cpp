452be4ff982f85127ff1299af667d1c7682a0b2a
Mohamed Mustafa <mohamed_czar@yahoo.com>
2013-04-07 19:03:32 +0200

    Test

--- src/test.cpp
+++ src/test.cpp
@@ -7,12 +7,39 @@
 
 #include <iostream>
 
+#include <bitset>
+#include "base.h"
+#include "graph.h"
+#include "dfa.h"
+
 using namespace std;
 
 int main() {
-	cout << "raed bub" <<endl;
-	cout<<"al3ab baleh"<<endl;
+	graph g;
+	g.insertNode();
+	g.insertNode();
+	g.insertNode();
+	g.insertNode();
+	g.insertNode();
+
+	bs bitset1;
+	bitset1.set(1, 1);
+
+	g.insertEdge(0, 1, bitset1, false, "");
+	g.insertEdge(0, 3, bitset1, false, "");
+
+	bitset1.reset();
+	bitset1.set('1', 1);
+
+	g.insertEdge(1, 2, bitset1, true, "number");
+	bitset1.reset();
+	bitset1.set('a', 1);
+	g.insertEdge(3, 4, bitset1, true, "id");
+
+	dfa a(g);
+	vector<int> res = a.epsilon_closure(0);
+	for (unsigned int i = 0; i < res.size(); ++i) {
+		cout << res[i] << endl;
+	}
 	return 0;
 }
-
-
